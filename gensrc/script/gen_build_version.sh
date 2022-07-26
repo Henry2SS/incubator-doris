f3a3a6b8031ecee289731e44ed7b9ee5dc69d668
caiconghui <caiconghui1@jd.com>
2022-07-26 11:27:58 +0800

    [JD][Release] Release version of jd-1.1.7 of Doris

--- gensrc/script/gen_build_version.sh
+++ gensrc/script/gen_build_version.sh
@@ -25,7 +25,7 @@
 # contains the build version based on the git hash or svn revision.
 ##############################################################
 
-build_version="jd-1.1.6-release"
+build_version="jd-1.1.7-release"
 
 unset LANG
 unset LC_CTYPE
